fd8f58b7f7a92e9278ff37580d2de6c51db49ed5
s20454userpjatk <127026669+s20454userpjatk@users.noreply.github.com>
2024-06-26 12:53:45 +0200

    Update nodes.py (model-evaluation)

--- churn-modelling-kedro/src/churn_modelling_kedro/pipelines/model_evaluation/nodes.py
+++ churn-modelling-kedro/src/churn_modelling_kedro/pipelines/model_evaluation/nodes.py
@@ -1,15 +1,12 @@
 import pandas as pd
-import wandb
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 
 def evaluate_model(predictions: pd.DataFrame,
     project_name='Kedro-SUML-Test-Autogluon',
     learning_rate=0.01,
     epochs=10,
     log_additional_info=True) -> (pd.DataFrame, pd.DataFrame):
-    # Initialize WandB run
-    run = wandb.init(project=project_name, config={"learning_rate": learning_rate, "epochs": epochs})
 
     try:
     # Calculating metrics
@@ -24,16 +21,6 @@ def evaluate_model(predictions: pd.DataFrame,
             rownames=['Actual'],
             colnames=['Predicted']
         )
-
-        # Log metrics
-        if log_additional_info:
-            wandb.log({"conf_mat": wandb.plot.confusion_matrix(probs=None, y_true=predictions['Exited'].values,
-                                                               preds=predictions['Prediction'].values,
-                                                               class_names=['0', '1']),
-                       "accuracy": accuracy,
-                       "precision": precision,
-                       "recall": recall,
-                       "f1_score": f1})
 
         return pd.DataFrame({
             'accuracy': [accuracy],
