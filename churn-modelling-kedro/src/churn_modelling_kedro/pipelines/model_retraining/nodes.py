e6d0e01ba9a6167897f0299400b8fff2f653daa6
Konrad Reperowski <konrad@webalize.me>
2024-06-28 18:47:23 +0200

    Remove unused code

--- churn-modelling-kedro/src/churn_modelling_kedro/pipelines/model_retraining/nodes.py
+++
@@ -1,4 +0,0 @@
-"""
-This is a boilerplate pipeline 'model_retraining'
-generated using Kedro 0.19.5
-"""
--- launcher.py
+++ launcher.py
@@ -1,21 +1,4 @@
 import subprocess
-import requests
-import time
-
-# Launch backend fast api
-backend_process = subprocess.Popen(["fastapi", "run", "fastapi/modelapi.py"])
-
-# Wait until the backend is ready
-while True:
-    try:
-        # Check if the backend is ready by sending a request to an endpoint
-        response = requests.get("http://localhost:8000/")
-        response.raise_for_status()
-        if response.status_code == 200:
-            break  # Backend is ready
-    except requests.RequestException:
-        pass  # Backend is not yet ready
-    time.sleep(1)
 
 # Launch frontend after backend is done
 subprocess.run(["streamlit", "run", "streamlit/app.py"])
